11553889e655a28dcb0442e0552e57d5539d18e8
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-07-15 08:27:45 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace custom_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
